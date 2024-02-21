570f9758ad10b90b6967589b16546c9178f0d35c
Irina <149883699+irinakravtsova@users.noreply.github.com>
2024-02-21 14:54:29 +0300

    Add files via upload

--- index1.js
+++ index1.js
@@ -3,13 +3,6 @@
 const inputNode = document.querySelector(".js-inputMovie");
 const btnAddNode = document.querySelector(".js-btn-new-movie");
 const listlNode = document.querySelector(".js-movies-list");
-const btnDeleteNode = document.getElementById(".js-btn-delete");
-const btnSelectedNode = document.querySelector(".js-btn-selected");
-
-console.log(btnDeleteNode);
-
-
-
 
 const moviesFromLStorage = JSON.parse(localStorage.getItem('movies'));
 let movies = [];
@@ -18,84 +11,129 @@ if (Array.isArray(moviesFromLStorage)) {
 }
 render();
 
-
 function getFromUser() {
   const movieInput = inputNode.value;
   const movie = movieInput.trim();
   return movie; 
 }
 
-function addMovie(movie) {
+function addMovies(movie) {
   movies.push(movie);
- 
- }
+}
 function getMovies() {
   return movies;
 }
 function saveMoviesLocalStorage() {
   localStorage.setItem('movies', JSON.stringify(movies)); 
 }
-function deleteHandler() {
-  alert('нажал')
-this.remove();
-}
+
+// function render() {
+//   const movies = getMovies();
+//   let movieHTML = '';
+//   movies.forEach(movie =>  {
+//     movieHTML += `
+//      <li id='movie' class="movie" >
+//       <div class="js-btn-selected movie-chek movie-chek_close" id="btn-selected"></div>
+//       <p class="js-movie-name" >${movie}</p>
+//       <button id='btmDelete' class="js-btn-delete"><img src="крестик.svg" alt=""></button>
+//      </li>
+//     `
+//   });
+
+//  listlNode.innerHTML = movieHTML;
+
+// }
+
 function render() {
   const movies = getMovies();
-  let movieHTML = '';
-  movies.forEach(movie =>  {
-    movieHTML += `
-     <li id='movie' class="movie" >
-      <div class="js-btn-selected movie-chek movie-chek_close" id="btn-selected"></div>
-      <p class="js-movie-name" >${movie}</p>
-      <button id='btmDelete' class="js-btn-delete"><img src="крестик.svg" alt="">  </button>
-     </li>
-    `
-  });
- 
- listlNode.innerHTML = movieHTML;
- btnDeleteNode.addEventListener('click', deleteHandler);
+  listlNode.innerHTML = "";
+  movies.forEach(function(movie){
+    const li = document.createElement('li');
+    li.className = "li";
 
- }
+    const movieName = document.createElement('div');
+    movieName.className = 'movieName';
+    
+    movieName.textContent = `${movie}`;
+     
+    const btnDelete = document.createElement('button');
+    btnDelete.className = "btn-delete";
+    const btnSelected = document.createElement('div');
+    btnSelected.className = "btn-selected";
+
+   li.append(movieName); 
+   li.append(btnDelete);
+   li.prepend(btnSelected);
+
+   listlNode.append(li);
+   
+   btnDelete.addEventListener('click', function() {
+    alert('ты уверен, что хочешь удалить этот фильм?');
+    li.remove();
+    // localStorage.removeItem('movies');
+   
+    } )
+    let selectedLi;
+    btnSelected.addEventListener('click', function() {
+      alert('ты действительно хочешь пометить этот фильм, как просмотренный?')
+      selectedLi = li;
+      selectedLi.className = 'close';
+      btnSelected.className = "btn-selected-close"
+
+
+   });
+
+
+  });
+}
+
+
 function clearInput() {
   inputNode.value = '';
  }
 
 function addHandler() {
   const movieFromUse = getFromUser();
-  addMovie(movieFromUse);
+  addMovies(movieFromUse);
+   render();
   saveMoviesLocalStorage();
-  render();
   clearInput();
  
  }
+//  function deleteAndSelecterHandler(event) {
+//   let target = event.target;
+// //   if (target.className != '"btn-delete') {
+// //     return;
+// //   } listItem.remove();
+// // }
 
-
-
-//  function deleteMovieHandler(event) {
-//   if (event.target.closest('.movies-list')) {
-//     alert('нажал')
-
-
-//     renderMovies();
-   
-//   }
-// }  
-
-// function selectedMovieHandler(event) {
-//   if (event.target.closest('.movie')) {
- 
-//   movieNode.className = 'movie_close'
-
-  
+//   if (event.target.closest('.movieslist')) {
+//     alert('ghbdtn')
+//  }
 // }
+//  function deleteHandler(event) {
+
+
+  // if (event.target.closest('.movies-list')) {
+  //   alert('не я')
+
+  // }
+// } 
+// let selectedLi;
+// // let movie = document.querySelector('movie')
+
+// function selectedHandler(event) {
+//   let target = event.target;
+//   selectedMovie(target);
 // }
- 
+// function selectedMovie() {
+//   selectedLi = li;
+//   selectedLi.className = 'close';
+// }
 
 btnAddNode.addEventListener('click', addHandler);
+// listlNode.addEventListener('click', selectedHandler);
 
-
-
-// moviesListNode.addEventListener('click', selectedMovieHandler)
 
 
 
